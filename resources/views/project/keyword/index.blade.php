65414dffd1f1907cd80d27a0e9733652373e9542
Sebastiaaan <sebastiaan36@gmail.com>
2024-05-19 19:15:11 +0200

    edit sortable.js

--- resources/views/project/keyword/index.blade.php
+++ resources/views/project/keyword/index.blade.php
@@ -62,6 +62,7 @@
     }
 
 </style>
+    <script src="https://keywords.been-vandam.nl/build/assets/js/sorttable.js"></script>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
