7b8ce5c22a1fb8d0035c85c30498fadb75eec0b5
Sebastiaaan <sebastiaan36@gmail.com>
2024-07-25 16:10:56 +0200

    Update create.blade.php

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -5,12 +5,12 @@
         </h2>
 
 
-        @if ($status)
+       @isset($status)
             <div class="bg-green-100 border border-green-400 pt-3 text-green-700 px-4 py-3 rounded relative" role="alert">
                 <strong class="font-bold">Project toegevoegd. Klik op dashboard om deze te bekijken.</strong>
                 <span class="block sm:inline">{{ session('status') }}</span>
             </div>
-        @endif
+        @endisset
 
         <form action="{{ route('project.store') }}" method="POST">
             @csrf
